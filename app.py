49df2b9f3a7eb40e81c9237713705ab192b2599f
Matt Howard <matt.howard91@gmail.com>
2022-06-28 15:15:35 +0100

    fixed not importing db when running app

--- app.py
+++ app.py
@@ -16,10 +16,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.secret_key = "matt"  # hide in a production environment
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all() # creates data.db (line 14) & all tables if not present
-
 jwt = JWT(app, authenticate, identity)  
 
 api.add_resource(Item, "/item/<string:name>")
